7f81b75d16e77f82db13a07913b69279b7afd72d
Ibrahim Mahmood <62712358+ibrahimm7004@users.noreply.github.com>
2025-02-12 05:56:37 +0500

    Delete M1-text-gen directory

--- M1-text-gen/user_input.py
+++
@@ -1,8 +0,0 @@
-# prompt = "We run a travel booking platform that helps users find the best flight and hotel deals. It’s perfect for frequent travelers, budget-conscious tourists, and spontaneous adventurers."
-location = "Pakistan"
-language = "Urdu"
-trendy_humour = True    # default set to True
-audience_type = ["Young Adults / Gen Z (18-24)", "Millennials (25-34)"]
-CTA = ["Comment"]
-type_post = True    # default set to True
-prompt = "We run a coffee shop in Lahore named Beano. We want funny, trendy tweets to attract customers."
